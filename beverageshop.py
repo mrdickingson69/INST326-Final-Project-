aef3a4d870bd5eabcb6666a4a7516be0e32e602d
matthewtemesgen1 <112912148+matthewtemesgen1@users.noreply.github.com>
2023-11-17 12:02:42 -0500

    Update beverageshop.py

--- beverageshop.py
+++ beverageshop.py
@@ -1,1 +1,20 @@
+class BeverageShop:
+  def __init__(self):
+        # Input from txt file "Bev Shop", bottom methods was originally based on hardcoded menu
 
+    def display_menu(self):
+        # Display the menu of the beverage shop
+        print("==== Beverage Menu ====")
+        for category, sizes in self.menu.items():
+            print(f"\n{category}:")
+            for size, price in sizes.items():
+                print(f"  {size}: ${price:.2f}")
+
+    def get_customer_age(self):
+        # Get the customers age to see if they're above or below the age of 21
+        try:
+            age = int(input("Please enter your age for age verification: "))
+            return age
+        except ValueError:
+            print("Invalid input. Please enter a valid age.")
+            return self.get_customer_age()
